214da9f96d996b506e336ac8e95289c603902e2d
Brigham French <bfrench@hse.k12.in.us>
2023-01-06 08:49:12 -0500

    Initial Commit

---
+++ FHSNewsIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FHSNewsIOS
+//
+//  Created by Brigham French on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
